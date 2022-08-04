965be01cac8df379b433f647aebfc3eac13f6405
Tatiana Gandra <tatianagandra78@gmail.com>
2022-08-04 14:58:56 -0300

    modified configuration

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -3,6 +3,10 @@ export enum RoutePath {
     LOGIN = '/login',
     SETTINGS = '/settings',
     SETTINGS_PRODUCTS = 'products',
-    SETTINGS_TABLES = 'tables',
+    SETTINGS_GAMES = 'games',
     SETTINGS_USERS = 'users',
   }
+
+  /**criando um arquivo com nomes da rotas
+   * exportando o caminho das rotas
+   * enum (constantes enomeadas)**/
