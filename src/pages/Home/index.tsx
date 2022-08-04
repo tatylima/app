2fa9872e30ce439fdb51763ac4fc91e0271a03d8
Tatiana Gandra <tatianagandra78@gmail.com>
2022-08-04 17:04:41 -0300

    app style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,34 @@
+import * as S from "./style"; //importando as configuraçoes, apelidando com "S",informando que esta no arquivo Syle.ts//
+import { ReactComponent as Search } from "assets/icons/search.svg";
 const Home = () => {
-    return(<div>Componente Home</div>);
-  }
-  
-  export default Home;
+  return (
+    <S.Home>
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Xbox Live</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                Aqui ficará a data
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo jogo" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Jogos</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+    </S.Home>
+  );
+};
+
+export default Home;
