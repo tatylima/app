38390a45082d208faadb2f4be09f6cccdb4557cd
Tatiana Gandra <tatianagandra78@gmail.com>
2022-08-04 16:09:55 -0300

    folder organization

---
+++ src/mocks/orders.ts
@@ -0,0 +1,35 @@
+export const orders = [
+	{
+		product: {
+			id:          '507f191e810c19729de860ea',
+			name:        'Alice',
+			description: 'Alice no pais das maravilhas',
+			image:       'assets/img/alice.png',
+			price:       40.69,
+		},
+		quantity: 1,
+		observation: '',
+	},
+	{
+		product: {
+			id:          '507f191e810c19729de860ea',
+			name:        'Assasins Creed Rogue',
+			description: 'Jogo de tiro',
+			image:       'assets/img/Assasins Creed Rogue.png',
+			price:       40.29,
+		},
+		quantity: 3,
+		observation: '',
+	},
+	{
+		product: {
+			id:          '507f191e810c19729de860ea',
+			name:        'DeadFall Adventure',
+			description: 'Tiro em primeira pessoa,jogo de ação.',
+			image:       'assets/img/DeadFall Adventure.png',
+			price:       30.29,
+		},
+		quantity: 1,
+		observation: '',
+	},
+];
---
+++ src/mocks/products.ts
@@ -0,0 +1,59 @@
+export const products = [
+	{
+		id:          '507f191e810c19729de860ea',
+		name:        'Alice',
+			description: 'Alice no pais das maravilhas',
+			image:       'assets/img/alice.png',
+			price:       40.69,
+		},
+	{
+		id:          '507f191e810c19729de860ea',
+		name:        'Assasins Creed Rogue',
+        description: 'Jogo de tiro',
+        image:       'assets/img/Assasins Creed Rogue.png',
+        price:       40.29,
+    },
+	{
+		id:          '507f191e810c19729de860ea',
+		name:        'DeadFall Adventure',
+        description: 'Tiro em primeira pessoa,jogo de ação.',
+        image:       'assets/img/DeadFall Adventure.png',
+        price:       30.29,
+    },
+	{
+		id:          '507f191e810c19729de860ea',
+		name:        'Fifa 19',
+		description: 'Futebol',
+		image:       '/assets/img/fifa 19.png',
+		price:       30.29,
+	},
+	{
+		id:          '507f191e810c19729de860ea',
+		name:'Kung-Fu-Panda-Confronto-de-Lendas-para-Xbox-360',    
+		description: 'Kung Fu Panda: Confronto das lendas levará os fãs em uma nova e empolgante aventura com o adorado guerreiro kung fu dos filmes Kung Fu Panda da DreamWorks, com ação intensa e explosiva! Com uma variedade de desafiadoras fases locais e multiplayer, cenários conhecidos e inéditos, e mais de 20 personagens conhecidos da série Kung Fu Panda, o jogo vai conquistar jogadores que gostam de brawlers acompanhados de um pouco de humor... e bolinhos! Com presença do elenco dos três filmes da série, o jogo se foca em torno de um torneio épico de artes-marciais, onde todos os lutadores competem pela glória. Conteúdo adicional, incluindo novos personagens e cenários serão acrescentados após o lançamento, via download.',
+
+		image:'/assets/img/Kung-Fu-Panda-Confronto-de-Lendas-para-Xbox-360.png',
+		price:       40.49,
+	},
+	{
+		id:          '507f191e810c19729de860ea',
+		name:        'Minecraft',
+		description: 'Com seu jogo aberto, mundo massivo e base de fãs dedicada, o Minecraft é uma experiência ricamente recompensadora – quando você pega o jeito. Com instruções fáceis de seguir, dicas e truques dos especialistas por trás do jogo, o Minecraft for Beginners ajudará você a sobreviver e prosperar. Você aprenderá a encontrar comida, construir um abrigo, minerar materiais e criar armaduras, espadas e outros equipamentos, além de obter informações privilegiadas sobre lugares para ir e os monstros que encontrará. O que você está esperando? Comece sua aventura no Minecraft hoje!',
+		image:       '/assets/img/minecraf.png',
+		price:       60.59,
+	},
+	{
+		id:          '507f191e810c19729de860ea',
+		name:        'NBA Live 15',
+		description: 'NBA LIVE 15 é um jogo de simulação de basquete onde os usuários disputam partidas competitivas com os times da NBA. Os usuários podem jogar com suas equipes ou jogadores favoritos, além de assumir o papel do general manager (GM) em vários modos como Tip Off, New Rising Star, LIVE Ultimate Team, Head 2 Head (Online), Learn LIVE e Dynasty. Durante o jogo o usuário tem controle total da frequência de faltas de defesa, de ataque, durante arremesso e intencionais. O jogador tem acesso a inúmeras opções de personalização como gerenciamento de elencos, opções de treino para cada time e visualização do livro de jogadas. Os modos disponíveis aos usuários são descritos em seguida: Tip Off - Esse modo possibilita selecionar rapidamente as equipes e disputar um jogo. Learn LIVE - Esse modo contém vários treinos e tutoriais para ajudar o usuário a dominar os controles e jogadas disponíveis no NBA Live 15. New Rising Star - Aqui o usuário pode criar um personagem e começar sua carreira como um astro em ascensão da NBA, iniciando com jogador universitário até chegar a profissional da NBA. Dynasty Mode - Nesse modo os usuários podem gerenciar a sua franquia (equipe) para criar uma dinastia na NBA. Head-to-Head- Neste modo, o usuário pode disputar partidas online contra outros jogadores de NBA Live 15. Take On Challenges - Possibilita ao usuário recriar alguns dos maiores momentos da temporada da NBA. LIVE Ultimate Team - Modo onde o usuário pode obter cartões que representam jogadores reais da NBA e usá-los para montar um time e competir online contra outras pessoas.',
+		image:       '/assets/img/NBA Live 15.png',
+		price:       49.90,
+	},
+	{
+		id:          '507f191e810c19729de860ea',
+		name:        'Sonic',
+		description: 'Sonic the Hedgehog',
+		image:       '/assets/img/sonic.png',
+		price:       30.49,
+	}
+]
