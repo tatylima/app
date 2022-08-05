eed894c758b0d02222131085a5baa53f1097c735
Tatiana Gandra <tatianagandra78@gmail.com>
2022-08-05 15:23:47 -0300

    feat conf navigation

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,8 +1,15 @@
 import logo from "assets/imgs/logo.png";
 import { ReactComponent as Logout } from "assets/icons/logout.svg";
 import * as S from "./style";
+import { RoutePath } from "types/routes";
+import { NavItem } from "./types";
 
-const Menu = () => {
+interface MenuProps {
+  active: RoutePath;
+  navItems: NavItem[];
+}
+
+const Menu = ({ active, navItems }: MenuProps) => {
   return (
     <S.Menu>
       <nav>
@@ -10,9 +17,13 @@ const Menu = () => {
         <S.MenuLogo>
           <img src={logo} alt="Logo" />
         </S.MenuLogo>
-        <S.MenuItem>
-          <S.MenuItemButton></S.MenuItemButton>
+        {navItems.map((item, index) => (     //fazer um map em navItems para que todos os botões (atuais e futuros) possam ser renderizados de forma correta na página Home//.
+        <S.MenuItem key={`MenuItem-${index}`} active={item.path === active}>             
+				<S.MenuItemButton active={item.path === active}>
+					{item.icon}  
+          </S.MenuItemButton>
         </S.MenuItem>
+        ))}      
       </nav>
       <S.MenuItemLogout>
         <Logout />
@@ -22,3 +33,6 @@ const Menu = () => {
 };
 
 export default Menu;
+/*Vamos passar duas propriedades dentro do MenuItem que serão: a chave (key) recebendo o index e o active recebendo a validação da rota do item com a rota informada pelo componente.
+
+*Além disso, item.icon serve para renderizar o ícone que configuramos dentro da interface NavItem*/
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,40 +1,42 @@
 import Menu from "components/Menu";
-import * as S from "./style"; //importando as configuraçoes, apelidando com "S",informando que esta no arquivo Syle.ts//
 import { ReactComponent as Search } from "assets/icons/search.svg";
+import * as S from "./style";
+import { RoutePath } from "types/routes";
+import { navigationItems } from "data/navigation";
 
 const Home = () => {
-  return (
-    <S.Home>
-      <Menu />
-      <S.HomeContent>
-        <header>
-          <S.HomeHeaderDetails>
-            <div>
-              <S.HomeHeaderDetailsLogo>Xbox Live</S.HomeHeaderDetailsLogo>
-              <S.HomeHeaderDetailsDate>
-                Aqui ficará a data
-              </S.HomeHeaderDetailsDate>
-            </div>
-            <S.HomeHeaderDetailsSearch>
-              <Search />
-              <input type="text" placeholder="Procure pelo jogo" />
-            </S.HomeHeaderDetailsSearch>
-          </S.HomeHeaderDetails>
-        </header>
-        <div>
-          <S.HomeProductTitle>
-            <b>Jogos</b>
-          </S.HomeProductTitle>
-          <S.HomeProductList>
-            <p>Lista de produtos aqui</p>
-          </S.HomeProductList>
-        </div>
-      </S.HomeContent>
-      <aside>
-        <p>Detalhes dos pedidos aqui</p>
-      </aside>
-    </S.Home>
-  );
+	return (
+		<S.Home>
+			<Menu active={RoutePath.HOME} navItems={navigationItems} />
+			<S.HomeContent>
+				<header>
+					<S.HomeHeaderDetails>
+						<div>
+							<S.HomeHeaderDetailsLogo>Xbox Live</S.HomeHeaderDetailsLogo>
+							<S.HomeHeaderDetailsDate>
+								Aqui ficará a data
+							</S.HomeHeaderDetailsDate>
+						</div>
+						<S.HomeHeaderDetailsSearch>
+							<Search />
+							<input type="text" placeholder="Procure pelo jogo" />
+						</S.HomeHeaderDetailsSearch>
+					</S.HomeHeaderDetails>
+				</header>
+				<div>
+					<S.HomeProductTitle>
+						<b>Jogos</b>
+					</S.HomeProductTitle>
+					<S.HomeProductList>
+						<p>Lista de produtos aqui</p>
+					</S.HomeProductList>
+				</div>
+			</S.HomeContent>
+			<aside>
+				<p>Detalhes dos pedidos aqui</p>
+			</aside>
+		</S.Home>
+	);
 };
 
 export default Home;
