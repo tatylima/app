46e0542fe12610e238b8238ea4f0f1edbbdf1ba9
Tatiana Gandra <tatianagandra78@gmail.com>
2022-08-07 17:29:46 -0300

    building order

--- src/components/OrderDetails/index.tsx
+++ src/components/OrderDetails/index.tsx
@@ -1,18 +1,38 @@
-import * as S from "./style";
+import ButtonLarge from "components/ButtonLarge";
 import ButtonToggle from "components/ButtonToggle";
+import OrderItemList from "components/OrderItemList";
+import * as S from "./style";
 
 const OrderDetails = () => {
-  return (
-    <S.OrderDetails>
-      <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
-      <S.OrderDetailsButtonGroup>
-        <ButtonToggle active={true} value="Entrega" />
-      </S.OrderDetailsButtonGroup>
-      <S.OrderDetailsList>
-        <p>Itens do pedido</p>
-      </S.OrderDetailsList>
-    </S.OrderDetails>
-  );
+	return (
+		<S.OrderDetails>
+			<S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
+			<S.OrderDetailsButtonGroup>
+				<ButtonToggle active={true} value="Entrega" />
+			</S.OrderDetailsButtonGroup>
+			<S.OrderDetailsList>
+				<OrderItemList
+					header={
+						<S.OrderDetailsListTitle>
+							<h4>Item</h4>
+							<h4>Qtd</h4>
+							<h4>Preço</h4>
+						</S.OrderDetailsListTitle>
+					}
+					list={"Lista de Itens"}
+					footer={
+						<S.OrderDetailsListFooter>
+							<S.OrderDetailsListFooterRow>
+								<span>Subtotal</span>
+								<span>R$ 10.00</span>
+							</S.OrderDetailsListFooterRow>
+							<ButtonLarge value="Continue para o pagamento" />
+						</S.OrderDetailsListFooter>
+					}
+				/>
+			</S.OrderDetailsList>
+		</S.OrderDetails>
+	);
 };
 
 export default OrderDetails;
