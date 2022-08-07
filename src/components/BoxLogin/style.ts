9d7d5038461b39385e6c62e68791a1913ba78cd2
Tatiana Gandra <tatianagandra78@gmail.com>
2022-08-07 13:37:45 -0300

    construction of the login page

--- src/components/BoxLogin/style.ts
+++ src/components/BoxLogin/style.ts
@@ -24,32 +24,19 @@ export const BoxLoginLogo = styled.div`
   	align-items: center;
   `}
 `;
-export const BoxLoginLogoText = styled.h1`
+export const BoxLoginLogoImage = styled.img`
+  ${() => css`
+  	width: 45%;
+  `}
+`;
+export const BoxLoginForm = styled.form`
   ${({ theme }) => css`
-  	font-family: ${theme.constants.logoFontFamily};
-  	font-size: 4em;
-  	line-height: 80px;
-  	font-weight: 400;
-  	text-transform: uppercase;
-  	margin: 20px 0;
-  	padding: 2.5px 0;
   	display: flex;
   	flex-direction: column;
-  	justify-content: center;
-  	border-top: 1px solid ${theme.colors.textColor};
-  	border-bottom: 1px solid ${theme.colors.textColor};
-  	text-align: end;
-  	width: calc(50% - 10px);
-  	span:first-child {
-  		border-top: 1px solid ${theme.colors.textColor};
-  		display: inline-block;
-  		letter-spacing: 9px;
-  	}
-  	span:last-child {
-  		border-bottom: 1px solid ${theme.colors.textColor};
-  		display: inline-block;
-  		letter-spacing: 9px;
-  		letter-spacing: 6px;
+  	gap: 15px;
+  	input {
+  		${theme.mixins.input()};
+  		color: ${theme.colors.textColor};
   	}
   `}
 `;
